5ff09064d99136dc9a68ead1c52c96461396d8d6
Gabrielle Hoover <grh39@drexel.edu>
2020-07-12 14:42:35 -0400

    Added color picker
    Styled for mobile
    Potential issue: The color picker wheel's width and height can only be specified in px, in js.

--- common/scripts/coloringBook.js
+++ common/scripts/coloringBook.js
@@ -4,6 +4,14 @@ picker_tool.addEventListener('mousedown', function() {
     openModal(this.id);
 });
 
+//initialize color picker with iro
+var colorPicker = new iro.ColorPicker("#picker", {
+  // Set the size of the color picker wheel. can only be modified in js in px
+  width: 200,
+  // Set the initial color to pure red
+  color: "#fff"
+});
+
 // Function displays selected modal. Accepts paramater of the clicked element's ID.
 function openModal(e) {
     var modal = document.getElementById(`${e}_modal`);
@@ -22,3 +30,8 @@ function openModal(e) {
         }
     }
 };
+
+//display hex code when picker is used
+colorPicker.on('color:change', function(color) {
+    document.getElementById('hexchange').innerHTML=(color.hexString);
+});
